28f54cbbd77095067a2e596537f7c34995d55205
sangbinlee <sangbinlee9@gmail.com>
2022-05-14 16:56:04 +0900

    소스 푸시하고 브랜치에 카피하면 홈페이지 수정확인 가능함

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-	<h1>소스 수정 후 배포222  Hello {name}!</h1>
+	<h1>푸시하고 gh-pages 브랜치 에 소스 카피    Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
